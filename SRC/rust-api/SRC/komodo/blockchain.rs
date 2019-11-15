ee83c5697766614e94e9c3b1884fe369132ce888
Ashish Mainali <ashish.mainali@ymail.com>
2019-11-14 19:31:31 -0600

    Blockchain function added

---
+++ SRC/rust-api/SRC/komodo/blockchain.rs
@@ -0,0 +1,285 @@
+use super:: komodorpcutil;
+use komodorpcutil::KomodoRPC;
+
+
+
+//getblockchaininfo
+
+pub fn coin_supply(
+	some_user: komodorpcutil::KomodoRPC,
+	height_supplied: Option<u32>)  
+	-> Result<(), reqwest::Error> {
+	/*
+	The coinsupply method takes input of optional paramater called height
+	that indicates the block height. If no height is provided, the function
+	uses a default value of current blockchain's height.
+	Function 
+    @param height: (32 bit initeger, optional) the desired block height
+    @return: JSON string containing:
+        "result" (string) whether the request was successful
+        "coin" (string) the ticker symbol of the coin for asset chains,
+        "height" (integer) the height of this coin supply data
+        "supply" (float) the transparent coin supply
+        "zfunds" (float) the shielded coin supply (in zaddrs)
+        "sprout" (float) the sprout coin supply (in zcaddrs)
+        "total" (float) the total coin supply,
+			i.e. sum of supply + zfunds
+	*/
+	let method_name:String = String::from("coinsupply");
+	let method_body:String;
+	
+	let height = height_supplied.unwrap_or(0); //Default value is 0
+	if height > 0 
+	
+	{
+		method_body = String::from(format!("[\"{0}\"]",height));
+	}
+	else
+	{
+		method_body = String::from("[]");
+	}
+	print!("{}\n",method_body );
+	let data:String = String::from (komodorpcutil::generate_body(some_user.clone(),method_name,method_body));
+	print!("{}\n\n",data);
+	let result =komodorpcutil::request( some_user.clone(), data);
+    return result;
+}
+
+pub fn get_best_block_hash(
+	some_user: komodorpcutil::KomodoRPC) 
+	->Result<(), reqwest::Error>
+{
+    let method_name:String = String::from("getbestblockhash");
+	let method_body:String = String::from("[]");
+	let data:String = String::from (komodorpcutil::generate_body(some_user.clone(),method_name,method_body));
+	let result =komodorpcutil::request( some_user.clone(), data);
+    return result;
+
+}
+
+
+
+
+
+pub fn get_blockchain_info(
+	some_user: komodorpcutil::KomodoRPC) 
+->Result<(), reqwest::Error>
+{
+    let method_name:String = String::from("getblockchaininfo");
+	let method_body:String = String::from("[]");
+	let data:String = String::from (komodorpcutil::generate_body(some_user.clone(),method_name,method_body));
+	let result =komodorpcutil::request( some_user.clone(), data);
+    return result;
+
+}
+
+
+/*************      N		E		W			F	U	N	C	T	I	O	N	S			************/       
+
+pub fn get_block_count(
+	SomeUser: komodorpcutil::KomodoRPC) 
+	->Result<(), reqwest::Error>
+{
+    let method_name:String = String::from("getblockcount");
+	let method_body:String = String::from("[]");
+	let data:String = String::from (komodorpcutil::generate_body(SomeUser.clone(),method_name,method_body));
+	let result =komodorpcutil::request( SomeUser.clone(), data);
+    return result;
+
+}
+
+pub fn get_block_hash(
+	SomeUser: komodorpcutil::KomodoRPC
+	,index:u32) 
+	->Result<(), reqwest::Error>
+{
+    let method_name:String = String::from("getblockckhash");
+	let method_body:String = String::from(format!("[{:?}]",index));
+	let data:String = String::from (komodorpcutil::generate_body(SomeUser.clone(),method_name,method_body));
+	let result =komodorpcutil::request( SomeUser.clone(), data);
+    return result;
+}
+
+pub fn get_chain_tips(
+	SomeUser: komodorpcutil::KomodoRPC) 
+	->Result<(), reqwest::Error>
+{
+    let method_name:String = String::from("getchaintips");
+	let method_body:String = String::from("[]");
+	let data:String = String::from (komodorpcutil::generate_body(SomeUser.clone(),method_name,method_body));
+	let result =komodorpcutil::request( SomeUser.clone(), data);
+    return result;
+}
+
+pub fn get_chain_tx_stats(
+	SomeUser: komodorpcutil::KomodoRPC, 
+	n_blocks:Option<u32>, 
+	block_hash:Option<String>) 
+	->Result<(), reqwest::Error>
+{
+	let method_body:String;
+	let temp_nblocks = n_blocks.unwrap_or(0); //Default value is 0
+	let temp_block_hash:String = block_hash.unwrap_or("".to_string());
+    if (temp_nblocks > 0) && !(temp_block_hash.is_empty())
+    {
+		method_body = String::from("[\"blockhash\": \"") + &temp_block_hash.to_string()+ &String::from(" \", \"nblocks\" : ") + &temp_nblocks.to_string() + &String::from("]");
+    }
+    else if (temp_nblocks <= 0) && !(temp_block_hash.is_empty())
+    {
+		method_body = String::from("[\"blockhash\": \"") + &temp_block_hash.to_string()+ &String::from(" \"]");
+	}
+	else if (temp_nblocks > 0) && (temp_block_hash.is_empty())
+    {
+        method_body = String::from("[\"nblocks\": ") + &temp_nblocks.to_string()+  &String::from("]");// &String::from(format!("[/"{0}/"]",temp_top));
+	}
+	else
+	{
+		method_body = String::from("[]");
+	}
+    let method_name:String = String::from("getchaintxstats");
+	let data:String = String::from (komodorpcutil::generate_body(SomeUser.clone(),method_name,method_body));
+	let result =komodorpcutil::request( SomeUser.clone(), data);
+    return result;
+}
+
+
+pub fn get_difficulty(
+	SomeUser: komodorpcutil::KomodoRPC) 
+->Result<(), reqwest::Error>
+{
+    let method_name:String = String::from("getdifficulty");
+	let method_body:String = String::from("[]");
+	let data:String = String::from (komodorpcutil::generate_body(SomeUser.clone(),method_name,method_body));
+	let result =komodorpcutil::request( SomeUser.clone(), data);
+    return result;
+}
+
+
+pub fn get_last_segid_stakes(
+	SomeUser: komodorpcutil::KomodoRPC,
+	depth:u32) 
+	->Result<(), reqwest::Error>
+{
+    let method_name:String = String::from("getlastsegidstakes");
+	let method_body:String = String::from(format!("[{}]",depth));
+	let data:String = String::from (komodorpcutil::generate_body(SomeUser.clone(),method_name,method_body));
+	let result =komodorpcutil::request( SomeUser.clone(), data);
+    return result;
+}
+
+
+pub fn get_mempool_info(
+	SomeUser: komodorpcutil::KomodoRPC) 
+	->Result<(), reqwest::Error>
+{
+    let method_name:String = String::from("getmempoolinfo");
+	let method_body:String = String::from("[]");
+	let data:String = String::from (komodorpcutil::generate_body(SomeUser.clone(),method_name,method_body));
+	let result =komodorpcutil::request( SomeUser.clone(), data);
+    return result;
+}
+
+pub fn get_raw_mempool(
+	SomeUser: komodorpcutil::KomodoRPC, 
+	verbose:Option<bool>) 
+	->Result<(), reqwest::Error>
+{
+	let temp_verbose:bool = verbose.unwrap_or(false);
+	let method_name:String = String::from("getrawmempool");
+	
+	let method_body:String;
+	if temp_verbose
+	{
+		method_body = String::from("[true]");
+	}
+	else
+	{
+		method_body = String::from("[false]");
+	}
+	let data:String = String::from (komodorpcutil::generate_body(SomeUser.clone(),method_name,method_body));
+	let result =komodorpcutil::request( SomeUser.clone(), data);
+    return result;
+}
+
+
+pub fn get_spent_info(
+	SomeUser: komodorpcutil::KomodoRPC, 
+	tx_id:String, 
+	index: u32) 
+	->Result<(), reqwest::Error>
+{
+	let method_body:String;
+    method_body = String::from("[\"txid\": \"") + &tx_id.to_string()+ &String::from(" \", \"index\" : ") + &index.to_string() + &String::from("]");
+    let method_name:String = String::from("getchaintxstats");
+	let data:String = String::from (komodorpcutil::generate_body(SomeUser.clone(),method_name,method_body));
+	let result =komodorpcutil::request( SomeUser.clone(), data);
+    return result;
+}
+
+pub fn get_tx_out(
+	SomeUser: komodorpcutil::KomodoRPC, 
+	tx_id:String, 
+	vout: u32, 
+	include_mempool:Option<bool>) 
+	->Result<(), reqwest::Error>
+{
+	/*
+	Arguments
+	Name	Type	Description
+	"txid"	(string, required)	the transaction id
+	vout	(numeric, required)	the vout value
+	includemempool	(boolean, optional)	whether to include the mempool
+	*/
+	 let bool_include_mempool:bool = include_mempool.unwrap_or(false);
+	 let method_body: String;
+	if bool_include_mempool == true
+	{
+		method_body = String::from("[\"txid\": \"") + 
+		&tx_id.to_string()+ &String::from(" \", \"vout\" : ") + 
+		&vout.to_string() + &String::from(" , \"includemempool\" : ") + 
+		&String::from("true") + &String::from("\" ]");
+
+	}
+	else
+	{
+		method_body = String::from("[\"txid\": \"") + 
+		&tx_id.to_string()+ &String::from(" \", \"vout\" : ") + 
+		&vout.to_string() + &String::from(" ]");
+
+	}
+    let method_name:String = String::from("getchaintxstats");
+	let data:String = String::from (komodorpcutil::generate_body(SomeUser.clone(),method_name,method_body));
+	let result =komodorpcutil::request( SomeUser.clone(), data);
+    return result;
+}
+
+pub fn get_tx_out_proof(
+	SomeUser: komodorpcutil::KomodoRPC, 
+	tx_id:String, 
+	block_hash:Option<String>) 
+	->Result<(), reqwest::Error>
+{
+	/*
+	Arguments
+	Name	Type	Description
+	"txid"	(string)	a transaction hash
+	"blockhash"	(string, optional)	if specified, the method looks for the relevant transaction id in this block hash
+	*/
+	 let temp_block_hash:String = block_hash.unwrap_or("".to_string());
+	 let method_body: String;
+	 if (!temp_block_hash.is_empty())
+	{
+		method_body = String::from("[\"txid\": \"") + 
+		&tx_id.to_string()+ &String::from(" \", \"blockhash\" : ") + 
+		&block_hash.to_string() + &String::from(" ]");
+
+	}
+	else 
+	{
+
+	}
+    let method_name:String = String::from("getchaintxout");
+	let data:String = String::from (komodorpcutil::generate_body(SomeUser.clone(),method_name,method_body));
+	let result =komodorpcutil::request( SomeUser.clone(), data);
+    return result;
+}
--- SRC/rust-api/src/komodorpcutil.rs
+++ SRC/rust-api/src/komodorpcutil.rs
@@ -1,152 +1,196 @@
-
-
 extern crate reqwest;
 use reqwest::header::*;
 
-
 #[derive(Debug,Clone)]
+/**
+ * Struct Komodo RPC holds the configuration information 
+ * to make connection to the komodo Daemon
+ */
 pub struct KomodoRPC
 {
-    
-    rpc_address: String,
-    rpc_port: i32,
-	req_method: String,
-    rpc_username: String,
-    rpc_password: String,
-	json_rpc_ver: String,
-    rpc_id: String
-	
+	rpc_address: String, 		// the ip address/URL of the komodo daemon
+    rpc_port: i32, 				// RPC port number of Komodo Daemon Server
+	req_method: String,			// Request method such as GET, POST	
+    rpc_username: String,		// Username of the account in Komodo Daemon for authentication
+    rpc_password: String,		// Password of the account in Komodo Daemon for authentication
+	json_rpc_ver: String,		// JSON version
+    rpc_id: String				// RPC request ID for the connection
 }
 
 
+/**
+ * Implementing methods for Komodo RPC Configuration Struct. 
+ * The methods will read and write data to the Komodo RPC Configuration Struct instance.
+ *  */
 impl KomodoRPC
 {
+	/**
+	 * The default method of Komodo RPC initializes Komodo RPC with 
+	 * default address, port, method, username, passoword, RPC version, RPC ID
+	 */
 	pub fn default() -> KomodoRPC {
         KomodoRPC {
-        /*
-		rpc_address: String::from("127.0.0.1"),
-        rpc_port: 7777,
-        req_method: String::from("POST"),
-        rpc_username:String::from(""),
-        rpc_password:String::from(""),
-        json_rpc_ver: String::from("1.0"),
-        rpc_id: String::from("curltest"),
-		*/
-		rpc_address: String::from("127.0.0.1"),
-        rpc_port: 8158,
-        req_method: String::from("POST"),
-        rpc_username:String::from("user2509089925"),
-        rpc_password:String::from("passf2e2f61a68e19ce65fe8211aa5d42163fb5c507c8d8d63dabd2c3f66c5358527eb"),
-        json_rpc_ver: String::from("1.0"),
-        rpc_id: String::from("curltest"),
-
+        	rpc_address: String::from("127.0.0.1"),
+			rpc_port: 8158,
+	        req_method: String::from("POST"),
+	        rpc_username:String::from("user2509089925"),
+	        rpc_password:String::from("passf2e2f61a68e19ce65fe8211aa5d42163fb5c507c8d8d63dabd2c3f66c5358527eb"),
+	        json_rpc_ver: String::from("1.0"),
+	        rpc_id: String::from("curltest"),
         }
     }
-	
+
+	/**
+	 * The new method of KomodoRPC takes input paramaters from the calee
+	 * and assigns the input to the struct Komodo RPC. 
+	 */
 	pub fn new(rpc_address: String, 
 	rpc_port: i32, 
 	req_method: String,
-    rpc_username: String, 
+	rpc_username: String, 
 	rpc_password: String,  
 	json_rpc_ver: String, 
 	rpc_id: String) -> KomodoRPC
 	{
-		
 		KomodoRPC
 		{
-			
+			//Assigning the paramaters to the struct 
 			rpc_address: rpc_address,
 			rpc_port: rpc_port,
 			req_method: req_method,
 			rpc_username: rpc_username,
 			rpc_password: rpc_password,
 			json_rpc_ver: json_rpc_ver,
-			rpc_id: rpc_id
-			
+			rpc_id: rpc_id,
 		}
 	}
-		// example helper method for rpc_address
-	pub fn GetRPCAddress(&self) -> String
+
+	/**
+	 *Function Name: get_rpc_address
+	 *@params: the instance of the struct KomodoRPC
+	 *Output: string that has RPC address such as URL or IP address
+	 */
+	pub fn get_rpc_address(&self) -> String
 	{
 		return self.rpc_address.to_string();
 	}
 
-	pub fn GetRPCId(&self) -> String
+	/**
+	 *Function Name: get_rpc_id
+	 *@params: the instance of the struct KomodoRPC
+	 *Output: string that has RPC ID
+	 */
+	pub fn get_rpc_id(&self) -> String
 	{
 		return self.rpc_id.to_string();
 	}
-	
-	pub fn GetRequestURL(&self) -> String
+
+	/**
+	 *Function Name: get_request_url
+	 *@params: the instance of the Struct KomodoRPC
+	 *Output: string that has Request URL and port number in format http://72.168.1.44:22
+	 */
+	pub fn get_request_url(&self) -> String
 	{
 		return String::from(format!("http://{0}:{1}/", self.rpc_address, self.rpc_port));
 	}	
 	
-	pub fn GetUsername(&self) -> String
+	/**
+	 *Function Name: get_username
+	 *@params: the instance of the struct KomodoRPC
+	 *Output: string that has rpc_address
+	 */
+	pub fn get_username(&self) -> String
 	{
 		return self.rpc_username.to_string();
 	}
-	pub fn GetPassword(&self) -> String
+
+	/**
+	 *Function Name: get_password
+	 *@params: the instance of the struct KomodoRPC
+	 *Output: string that has the password
+	 */
+	pub fn get_password(&self) -> String
 	{
 		return self.rpc_password.to_string();
 	}
 
-	pub fn GetRequestMetadata(&self) -> String
+	/**
+	 *Function Name: get_request_metadata
+	 *@params: the instance of the struct KomodoRPC
+	 *Output: string that has RPC request meta data in following format
+	 *{"jsonrpc": "1", "id" : "curltest"}
+	 */
+	pub fn get_request_metadata(&self) -> String
 	{
-		let ReqMetadata:String = format!("{{\"jsonrpc\": \"{0}\", \"id\":\"{1}\", ", self.json_rpc_ver, self.rpc_id);
-		return ReqMetadata;
+		let request_metadata:String = format!("{{\"jsonrpc\": \"{0}\", \"id\":\"{1}\", ", self.json_rpc_ver, self.rpc_id);
+		return request_metadata;
 	}
-	pub fn getJSONRPCver(&self) -> String
+	/**
+	 *Function Name: get_json_ver
+	 *@params: the instance of the struct KomodoRPC
+	 *Output: string containing the json version for the request reply
+	 */
+	pub fn get_json_ver(&self) -> String
 	{
-		let temp_JSON_RPC_Ver = format!("{}",self.json_rpc_ver);
-		return temp_JSON_RPC_Ver;
+		let temp_json_rpc_ver = format!("{}",self.json_rpc_ver);
+		return  temp_json_rpc_ver;
 	}
-
-	
 
 }
 
-pub fn GenerateBody(SomeUser: KomodoRPC, method_name:String , method_parameter:String) -> String
+/**
+	 *Function Name: generate_body
+	 *@params: KomodoRPC - the instance of the struct KomodoRPC
+	 			method_name - name of the method to query to the Komodo Daemon
+				 method_paramaters - paramaters for the method passed 
+	 *Output: the function generates an output for the HTTP request method encoding
+	 * 			the method, paramaters and metadata generated by get_request_metadata() function.
+	 */
+pub fn generate_body(some_user: KomodoRPC, method_name:String , method_parameter:String) -> String
 	{
 		let method = format!("\"method\":\"{0}\",", method_name);
 		let paramater = format! ("\"params\": {0} }}",method_parameter);
-	    let mut body:String = SomeUser.GetRequestMetadata();
+	    let mut body:String = some_user.get_request_metadata();
 		body = body + &method + &paramater;
 		//body.push_str(method.to_string());
 		//body.push_str(paramater);
 		//print!("{:?}",body);
 		return body;
-		
 	}
 
+	/**
+	 *Function Name: request
+	 *@params: some_user - an instance of the KomodoRPC struct containing the configuration for Komodo RPC Request
+	 			body_input - it has the body of the request provided by the get_body() method.
+	 *Output: Result of the request 
+	 			ie. JSON containing the the output result from the request 
+				 	Error Codes - containing the error in the HTTP request
+	 */
 
-
-pub fn request(SomeUser: KomodoRPC, body_input: String) -> Result<(), reqwest::Error> {
-
-    
+pub fn request(some_user: KomodoRPC, body_input: String) -> Result<(), reqwest::Error> {
+	if some_user.get_username().trim() !=""
+	{	
     let mut headers = HeaderMap::new();
     headers.insert(CONTENT_TYPE, "text/plain;".parse().unwrap());
-
     // assume this is from the struc
-	let someJSONRPCVer = String::from(SomeUser.getJSONRPCver());
-	let someRPCReqID = String::from(SomeUser.GetRPCId());
-
-    let reqMeta = format!("\"jsonrpc\":\"{0}\", \"id\":\"{1}\"", someJSONRPCVer, someRPCReqID);
-    //                  "jsonrpc": "1.0", "id":"curltest"
-    //println!("\n\nthe value of asdf is: {}\n", reqMeta);
-	let  URL_POST= SomeUser.GetRequestURL();
-
-
+	let some_json_rpc_ver = String::from(some_user.get_json_ver());
+	let some_rpc_id = String::from(some_user.get_rpc_id());
+	let  url_post= some_user.get_request_url();
     let res = reqwest::Client::new()
-        .post(&URL_POST)
-        .basic_auth(SomeUser.GetUsername(), Some(SomeUser.GetPassword()))
+        .post(&url_post)
+        .basic_auth(some_user.get_username(), Some(some_user.get_password()))
         .headers(headers)
         //.body("{\"jsonrpc\": \"1.0\", \"id\":\"curltest\", \"method\": \"getwalletinfo\", \"params\": [] }")
         .body(body_input)        
         .send()?
         .text()?;
-    println!("{}", res);
+    println!("the result is{}", res);
     Ok(())
+	}
+	else
+	{
+		println!("Error");
+		return Ok(());
+	}
 }
-
-
-
